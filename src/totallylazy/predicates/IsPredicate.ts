2d0cbc78ab5e8470c80b67fdbc726b55f645e1f2
Daniel Worthington-Bodart <dan@bodar.com>
2023-09-04 11:35:00 +0100

    update jsdoc comment

--- src/totallylazy/predicates/IsPredicate.ts
+++ src/totallylazy/predicates/IsPredicate.ts
@@ -6,6 +6,9 @@ export interface IsPredicate<A> extends DescriptivePredicate<A> {
 
 /**
  * Returns a predicate that does a strict equality check against the value
+ * @template T
+ * @param {T} value
+ * @returns {IsPredicate<T>}
  */
 export const is = <A>(value: A): IsPredicate<A> => Object.assign((a: A) => a === value, {
     value: value,
