6b0f8e336bcf6555d9b46b09bdc2605f24aaaf96
Daniel Worthington-Bodart <dan@bodar.com>
2023-09-04 16:03:33 +0100

    fix test

--- test/totallylazy/testing/assertThat.test.ts
+++ test/totallylazy/testing/assertThat.test.ts
@@ -4,17 +4,13 @@ import {is} from "../../../src/totallylazy/predicates/IsPredicate.ts";
 import {AssertionError} from "https://deno.land/std@0.200.0/assert/assertion_error.ts";
 
 Deno.test("assertThat", async (context) => {
-    await (async (it) => {
-        const actual = 2;
+    const actual = 2;
 
-        await context.step('does not throw when predicate matches', () => {
-            assertThat(actual, is(actual));
-        });
+    await context.step('does not throw when predicate matches', () => {
+        assertThat(actual, is(actual));
+    });
 
-        await context.step('does throw when predicate does not match', () => {
-            assertThrows(() => assertThat(actual, is(3)), AssertionError, "assertThat(2, is(3))");
-        });
-    })(async (test, inner) => {
-        await context.step(test, inner)
+    await context.step('does throw when predicate does not match', () => {
+        assertThrows(() => assertThat(actual, is(3)), AssertionError, "assertThat(2, is(3))");
     });
 });
