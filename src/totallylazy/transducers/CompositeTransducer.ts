5b65b03a1f91b4e396b14aed14c3a226645d50db
Server <server@bodar.com>
2023-09-11 11:58:26 +0100

    Added CompositeTransducer

---
+++ src/totallylazy/transducers/CompositeTransducer.ts
@@ -0,0 +1,41 @@
+import {Transducer} from "./Transducer.ts";
+
+/**
+ * A transducer that applies the given transducers in order
+ */
+export interface CompositeTransducer<A, B> extends Transducer<A, B> {
+     readonly transducers: readonly Transducer<any, any>[];
+}
+
+/**
+ * Creates a CompositeTransducer that applies the given transducers in order
+ */
+export function compose<A, B, C>(a: Transducer<A, B>, b: Transducer<B, C>): CompositeTransducer<A, C>;
+export function compose<A, B, C, D>(a: Transducer<A, B>, b: Transducer<B, C>, c: Transducer<C, D>): CompositeTransducer<A, D>;
+export function compose<A, B, C, D, E>(a: Transducer<A, B>, b: Transducer<B, C>, c: Transducer<C, D>, d: Transducer<D, E>): CompositeTransducer<A, E>;
+export function compose<A, B, C, D, E, F>(a: Transducer<A, B>, b: Transducer<B, C>, c: Transducer<C, D>, d: Transducer<D, E>, e: Transducer<E, F>): CompositeTransducer<A, F>;
+export function compose<A, B, C, D, E, F, G>(a: Transducer<A, B>, b: Transducer<B, C>, c: Transducer<C, D>, d: Transducer<D, E>, e: Transducer<E, F>, f: Transducer<F, G>): CompositeTransducer<A, G>;
+export function compose<A, B, C, D, E, F, G, H>(a: Transducer<A, B>, b: Transducer<B, C>, c: Transducer<C, D>, d: Transducer<D, E>, e: Transducer<E, F>, f: Transducer<F, G>, g: Transducer<G, H>): CompositeTransducer<A, H>;
+export function compose<A, B, C, D, E, F, G, H, I>(a: Transducer<A, B>, b: Transducer<B, C>, c: Transducer<C, D>, d: Transducer<D, E>, e: Transducer<E, F>, f: Transducer<F, G>, g: Transducer<G, H>, h: Transducer<H, I>): CompositeTransducer<A, I>;
+export function compose<A, Z>(...transducers: readonly [Transducer<A, any>, ...Transducer<any, any>[], Transducer<any, Z>]): CompositeTransducer<A, Z>;
+export function compose(...transducers: readonly Transducer<any, any>[]): CompositeTransducer<any, any> {
+    const flat = flatten(transducers);
+    return Object.assign((iterable: Iterable<any>) => flat.reduce((a, t) => t(a), iterable), {
+        transducers: flat,
+        toString: () => `transducers(${transducers})`
+    });
+}
+
+/**
+ * Checks if the given value is a CompositeTransducer
+ */
+export function isCompositeTransducer(value: any): value is CompositeTransducer<any, any> {
+    return value && typeof value === 'function' && Array.isArray(value.transducers);
+}
+
+/**
+ * Flattens the given transducers
+ */
+export function flatten(transducers: readonly Transducer<any, any>[]): readonly Transducer<any, any>[] {
+    return transducers.flatMap(t => isCompositeTransducer(t) ? flatten(t.transducers) : t);
+}
---
+++ test/totallylazy/transducers/CompositeTransducer.test.ts
@@ -0,0 +1,36 @@
+import {assertThat} from "../../../src/totallylazy/asserts/assertThat.ts";
+import {filter} from "../../../src/totallylazy/transducers/FilterTransducer.ts";
+import {equals} from "../../../src/totallylazy/predicates/EqualsPredicate.ts";
+import {is} from "../../../src/totallylazy/predicates/IsPredicate.ts";
+import {compose} from "../../../src/totallylazy/transducers/CompositeTransducer.ts";
+import {map} from "../../../src/totallylazy/transducers/MapTransducer.ts";
+
+Deno.test("CompositeTransducer", async (context) => {
+    const even = (x: number) => x % 2 === 0;
+    const f = filter(even);
+    const m = map(String);
+    const t = compose(f, m);
+
+    await context.step("can compose together multiple Transducers", () => {
+        assertThat(Array.from(t([1, 2, 3, 4, 5])), equals(['2', '4']));
+    });
+
+    await context.step("is inspectable", () => {
+        assertThat(t.transducers, equals([f, m]));
+    });
+
+    await context.step("is self describing", () => {
+        assertThat(t.toString(), is(`transducers(${f},${m})`));
+    });
+
+    await context.step("overload works with more than 2 arguments", () => {
+        const r = compose(filter(even), map(String), filter(v => v.length > 1), map(Number));
+        assertThat(Array.from(r([1, 2, 5, 10])), equals([10]));
+    });
+
+    await context.step("always flattens nested transducers", () => {
+        const r = compose(filter(even), map(String), compose(filter(v => v.length > 1), map(Number)));
+        assertThat(r.transducers.length, is(4));
+    });
+
+});
