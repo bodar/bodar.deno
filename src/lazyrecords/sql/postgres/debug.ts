1a25fdd8cbce75469b83bbfdf3c519e2f805a235
Server <server@bodar.com>
2023-10-07 09:59:47 +0100

    lazy-records: fix import

--- src/lazyrecords/sql/postgres/debug.ts
+++ src/lazyrecords/sql/postgres/debug.ts
@@ -3,6 +3,7 @@ import {Expression} from "../template/Expression.ts";
 import {Identifier} from "../template/Identifier.ts";
 import {escapeIdentifier, escapeLiteral} from "./escape.ts";
 import {Value} from "../template/Value.ts";
+import {Text} from "../template/Text.ts";
 
 export function debugQuery(sql: Sql): string {
     return sql.expressions.reduce((a: string, e: Expression) => {
