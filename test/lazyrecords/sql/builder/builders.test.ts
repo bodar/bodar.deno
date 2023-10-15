fa1d3e37a8151495ab9588925a438c14e6794c2f
Server <server@bodar.com>
2023-10-15 14:31:43 +0100

    lazyrecords: ensure null/undefined cases work

--- test/lazyrecords/sql/builder/builders.test.ts
+++ test/lazyrecords/sql/builder/builders.test.ts
@@ -4,7 +4,7 @@ import {definition, toSelect} from "../../../../src/lazyrecords/sql/builder/buil
 import {sql} from "../../../../src/lazyrecords/sql/template/Sql.ts";
 import {filter} from "../../../../src/totallylazy/transducers/FilterTransducer.ts";
 import {where} from "../../../../src/totallylazy/predicates/WherePredicate.ts";
-import {property, Property} from "../../../../src/totallylazy/functions/Property.ts";
+import {property} from "../../../../src/totallylazy/functions/Property.ts";
 import {map} from "../../../../src/totallylazy/transducers/MapTransducer.ts";
 import {select} from "../../../../src/totallylazy/functions/Select.ts";
 import { and } from "../../../../src/totallylazy/predicates/AndPredicate.ts";
@@ -14,11 +14,13 @@ import { or } from "../../../../src/totallylazy/predicates/OrPredicate.ts";
 interface Country {
     country_code: string;
     country_name: string;
+    optional?: string;
 }
 
 const country = definition<Country>("country");
 const countryCode = property<Country, 'country_code'>("country_code");
 const countryName = property<Country, 'country_name'>("country_name");
+const optional = property<Country, 'optional'>("optional");
 
 Deno.test("selectExpression", async (context) => {
     await context.step("works with just the definition", () => {
@@ -64,5 +66,15 @@ Deno.test("selectExpression", async (context) => {
     await context.step("can also write 'or' in infix style", () => {
         assertThat(sql(toSelect(country, filter(where(countryCode, is("GB")).or(where(countryName, is("United Kingdom")))))).toString(),
             is(`select all * from "country" where ("country_code" = 'GB' or "country_name" = 'United Kingdom')`));
+    });
+
+    await context.step("correctly handles null values", () => {
+        assertThat(sql(toSelect(country, filter(where(optional, is<string|undefined|null>(null))))).toString(),
+            is(`select all * from "country" where "optional" is null`));
+    });
+
+    await context.step("correctly transforms undefined values", () => {
+        assertThat(sql(toSelect(country, filter(where(optional, is<string|undefined>(undefined))))).toString(),
+            is(`select all * from "country" where "optional" is null`));
     });
 });
