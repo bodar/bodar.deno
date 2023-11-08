6a24de18a8f102ac07907066b29833f529161016
Server <server@bodar.com>
2023-11-08 10:26:00 +0000

    totalylazy: minor clean up

--- src/totallylazy/parsers/PatternParser.ts
+++ src/totallylazy/parsers/PatternParser.ts
@@ -4,15 +4,19 @@ import {fail} from "./Failure.ts";
 import {success} from "./Success.ts";
 import {View} from "./View.ts";
 
+export interface Matcher {
+    [Symbol.match](string: string): RegExpMatchArray | null;
+}
+
 export class PatternParser implements Parser<string, string> {
-    constructor(private readonly pattern: RegExp) {
+    constructor(private readonly matcher: Matcher) {
     }
 
     parse(input: View<string>): Result<string, string> {
         const source = String(input.toSource());
-        const match = source.match(this.pattern);
-        if (match === null) return fail(this.pattern, source);
-        const result = match[0];
+        const match = this.matcher[Symbol.match](source);
+        if (match === null) return fail(this.matcher, source);
+        const [result] = match;
         return success(result, input.slice(result.length));
     }
 }
--- test/totallylazy/parsers/PatternParser.test.ts
+++ test/totallylazy/parsers/PatternParser.test.ts
@@ -5,7 +5,7 @@ import {is} from "../../../src/totallylazy/predicates/IsPredicate.ts";
 import {PatternParser} from "../../../src/totallylazy/parsers/PatternParser.ts";
 
 Deno.test("PatternParser", async (context) => {
-    await context.step("can parse many", () => {
+    await context.step("can parse using a pattern", () => {
         const result = new PatternParser(/A+/).parse(view('AAABBBCCC'));
         assertThat(result.value, equals('AAA'));
         assertThat(result.remainder.toSource(), is('BBBCCC'));
