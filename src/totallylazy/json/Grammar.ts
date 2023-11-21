7fcc483577d17eb018a071fa3e3eb23ec61c034a
Server <server@bodar.com>
2023-11-21 18:57:44 +0000

    totallylazy: add json string grammar

--- src/totallylazy/json/Grammar.ts
+++ src/totallylazy/json/Grammar.ts
@@ -1,13 +1,36 @@
 import {Parser, parser} from "../parsers/Parser.ts";
 import {string} from "../parsers/StringParser.ts";
-import {next, returns} from "../parsers/parsers.ts";
+import {next, returns, surroundedBy} from "../parsers/parsers.ts";
 import {or} from "../parsers/OrParser.ts";
 import {map} from "../transducers/MapTransducer.ts";
 import {pattern} from "../parsers/PatternParser.ts";
+import {many} from "../parsers/ManyParser.ts";
+
+function unescape(escaped: 'b' | 'n' | 'r'| 't' | 'f'): string {
+    switch (escaped) {
+        case 'b': return '\b';
+        case 'n': return '\n';
+        case 'r': return '\r';
+        case 't': return '\t';
+        case 'f': return '\f';
+        default: throw new Error(`Should never happen: ${escaped}`);
+    }
+}
 
 export class Grammar {
     static null: Parser<string, null> = parser(string("null"), returns(null));
+
     static boolean: Parser<string, boolean> = parser(or(string("true"), string("false")), map(v => v === "true"));
 
-    static escapedCharacter: Parser<string, string> = parser(string('\\'), next(or(pattern(/["\\/bfnrt]/), pattern(/u[0-9a-fA-F]{4}/))));
+    static escapedCharacter: Parser<string, string> = parser(string('\\'), next(or(
+        pattern(/["\\/]/),
+        parser(pattern(/[bfnrt]/), map(unescape)),
+        parser(pattern(/u[0-9a-fA-F]{4}/), map(u => String.fromCharCode(parseInt(u.slice(1), 16))))
+        )));
+
+    static anyCodePoint: Parser<string, string> = pattern(/[^"\\]/);
+
+    static string: Parser<string, string> = parser(many(or(Grammar.escapedCharacter, Grammar.anyCodePoint)),
+        map(characters => characters.join('')), surroundedBy(string('"')));
 }
+
--- test/totallylazy/json/Grammar.test.ts
+++ test/totallylazy/json/Grammar.test.ts
@@ -17,8 +17,25 @@ Deno.test("Grammar", async (context) => {
     });
 
     await context.step("can parse escaped character", () => {
-        assertThat(Grammar.escapedCharacter.parse(view("\\\"")).value, is("\""));
-        assertThat(Grammar.escapedCharacter.parse(view("\\\\")).value, is("\\"));
+        assertThat(Grammar.escapedCharacter.parse(view('\\"')).value, is('"'));
+        assertThat(Grammar.escapedCharacter.parse(view('\\\\')).value, is('\\'));
         assertTrue(Grammar.escapedCharacter.parse(view('failure')) instanceof Failure);
+    });
+
+    await context.step("can parse escaped unicode character", () => {
+        assertThat(Grammar.escapedCharacter.parse(view('\\u03BB')).value, is(`λ`));
+        assertThat(Grammar.escapedCharacter.parse(view('\\u5c71')).value, is('山'));
+        assertThat(Grammar.escapedCharacter.parse(view('\\u7530')).value, is('田'));
+        assertThat(Grammar.escapedCharacter.parse(view('\\u3000')).value, is('　'));
+        assertThat(Grammar.escapedCharacter.parse(view('\\u7ae0')).value, is('章'));
+        assertThat(Grammar.escapedCharacter.parse(view('\\u96c4')).value, is('雄'));
+    });
+
+    await context.step("can parse string", () => {
+        assertThat(Grammar.string.parse(view('""')).value, is(''));
+        assertThat(Grammar.string.parse(view('"Word"')).value, is('Word'));
+        assertThat(Grammar.string.parse(view('"This is some \\" random string"')).value, is('This is some " random string'));
+        assertThat(Grammar.string.parse(view('"Text with unicode \u03BB after "')).value, is('Text with unicode λ after '));
+        assertTrue(Grammar.string.parse(view('failure')) instanceof Failure);
     });
 });
